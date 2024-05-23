d9755c6f67cf12de2eddf00bef6421004d3626ce
Eloiza <eloizaams@yahoo.com.br>
2024-05-23 11:05:14 -0300

    starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,8 +6,8 @@ public class Program {
 
 	public static void main(String[] args) {
 			
-		Position position = new Position (3,5);
-		System.out.println(position);
+		Board board = new Board[8][8];
+		
 				
 	}
 
